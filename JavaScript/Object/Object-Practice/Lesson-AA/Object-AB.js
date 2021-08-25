e1eef1c8194e356b1a00987baeae94ea007351db
Shameem Ahamed <rony.shameem@gmail.com>
2021-08-25 22:11:01 +0600

    javascript-object (lesson-aa) : push-5 (21-Aug-25)

--- JavaScript/Object/Object-Practice/Lesson-AA/Object-AB.js
+++ JavaScript/Object/Object-Practice/Lesson-AA/Object-AB.js
@@ -26,70 +26,133 @@ document.body.appendChild(outAB);
 // -> in an event, (this) referes to the (element) that received the event
 // -> method like (call() and apply()) can refer (this) to (any-object)
 
-function ExBA() {
-
-}
-
 ////////////////////////////////////////////////////////////////////////////////
-// # (this) in a method or object-method binding and (this) used alone
+// # (this) used alone | (this) in a method
+// when (this) used alone, the (owner) is the (globa-object)
+// -> so, (this) refers to the (global-object)
+// -> in a browser-window the (global-object) is ([object Window])
+// -> in (strict-mode), (this) also refers to the (global-object) is ([object Window])
+
 // in an object method, (this) refers to the (owner) of the method
 // -> object is the owner all of its methods
 
-// when (this) used alone, the (owner) is the (globa-object)
-// -> so, (this) refers to the (global-object)
-// -> in a browser window the (global-object) is ([object Window])
-// -> in (strict-mode), (this) also refers to the (global-object) is ([object Window])
-
-function ExBB() {
+function ExBA() {
   var varAA = {
     proOne: this,
 
     methodOne: function () {
-      outBA.innerHTML += '<br >methodOne (this) : ' + this;
-      outBA.innerHTML += '<br >proOne (this)    : ' + this.proOne;
+      outAB.innerHTML += '<br >methodOne (default-this) : ' + this;
+      outAB.innerHTML += '<br >proOne (this)    : ' + this.proOne;
+    },
+
+    methodTwo: function () {
+      'use strict';
+      outAB.innerHTML += '<br >methodTwo (strict-this) : ' + this;
+      outAB.innerHTML += '<br >proOne (this)    : ' + this.proOne;
     },
   };
 
+  // (this) used in object-method (default-mode)
+  // (this) used in object-method (strict-mode)
+  // (this) used as property-value (default-mode)
   varAA.methodOne();
+  varAA.methodTwo();
+  outAB.innerHTML += '<br >';
 }
 
+// (this) used alone (default-mode)
+outAB.innerHTML += '<br >used alone (default-this) : ' + this;
+outAB.innerHTML += '<br >';
+
 ////////////////////////////////////////////////////////////////////////////////
-// # (this) in a function (default) and (this) in a function (strict)
+// # (this) in a function (default) | (this) in a function (strict)
 // in a function, the owner of the function is the (default) binding for (this)
 // -> so, in a function, (this) refers to the global-object [object Window]
 
 // javascript (strict-mode) does not allow default binding
 // -> so, when used in a function, in (strict) mode, (this) is (undefined)
 
-function ExBC() {
+function ExBB() {
+  function nestedDefault() {
+    outAB.innerHTML += '<br >function (default-this) : ' + this;
+  }
 
+  function nestedStrict() {
+    'use strict';
+    outAB.innerHTML += '<br >function (strict-this) : ' + this;
+  }
+
+  nestedDefault();
+  nestedStrict();
+  outAB.innerHTML += '<br >';
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // # (this) in a event-handlers
 // in HTML event-handlers, (this) refers to the HTML element that received the event
 
-function ExBD() {
+function ExBC() {
 
+  // (this) refers to the (HTML-button-element) that received the (onclick) event
+  var element = document.createElement('button');
+  element.setAttribute('onclick', 'this.innerHTML="button"');
+  outAB.appendChild(element);
+
+  outAB.innerHTML += '<br >';
+
+  // without (this) HTML (onclick) event
+  var elementA = document.createElement('button');
+  elementA.setAttribute('onclick', 'document.getElementById("spanOne").innerHTML="<br >javascrip"');
+  outAB.appendChild(elementA);
+
+  var elementB = document.createElement('span');
+  elementB.setAttribute('id', 'spanOne');
+  outAB.appendChild(elementB);
+
+  outAB.innerHTML += '<br >';
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-// # explicit-function binding
+// # explicit-function binding | object-method binding
 // the (call() and apply()) methods are predefined javascript methods
 // -> they can both be used to call an object-method with another-object as argument
 
-function ExBE() {
+function ExBD() {
+  var varAA = {
+    proOne: 10,
+    proTwo: 20,
 
+    methodOne: function () {
+      outAB.innerHTML += '<br >varAA.methodOne (explicit-function-binding) ......';
+      outAB.innerHTML += '<br >varAA.methodOne : ' + this;
+      outAB.innerHTML += '<br >proOne + proTwo : ' + (this.proOne + this.proTwo);
+    },
+
+    methodTwo: function () {
+      outAB.innerHTML += '<br >varAA.methodTwo (object-method-binding) ...';
+      outAB.innerHTML += '<br >varAA.methodTwo : ' + this;
+      outAB.innerHTML += '<br >proOne + proTwo : ' + (this.proOne + this.proTwo);
+
+      outAB.innerHTML += '<br >varAA.methodTwo (using arguments) ...';
+      outAB.innerHTML += '<br >proOne + proTwo : ' + (arguments[0].proOne + arguments[0].proTwo);
+    },
+  };
+
+  var varAB = {
+    proOne: 11,
+    proTwo: 22,
+  };
+
+  varAA.methodOne.call(varAB);
+  varAA.methodTwo(varAB);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-outAB.innerHTML += 'ex-ba : javascript (this) keyword';
+outAB.innerHTML += '<br >ex-ba : (this) used alone (default) | (this) in method (default/strict)';
 outAB.innerHTML += '<br >'; ExBA();
-outAB.innerHTML += '<br >ex-bb : (this) in a method and when used alone (default and strict)';
+outAB.innerHTML += '<br>ex-bb : (this) in a function (default/strict)';
 outAB.innerHTML += '<br >'; ExBB();
-outAB.innerHTML += '<br>ex-bc : (this) in a function (default and strict)';
+outAB.innerHTML += '<br >ex-bc : (this) in a event-handlers';
 outAB.innerHTML += '<br >'; ExBC();
-outAB.innerHTML += '<br >ex-bd : (this) in a event-handlers';
+outAB.innerHTML += '<br >ex-bd : explicit-function binding | object-method binding';
 outAB.innerHTML += '<br >'; ExBD();
-outAB.innerHTML += '<br >ex-be : explicit-function binding';
-outAB.innerHTML += '<br >'; ExBE();
--- JavaScript/Object/Object-Practice/Lesson-AA/Object-AD.js
+++ JavaScript/Object/Object-Practice/Lesson-AA/Object-AD.js
@@ -15,7 +15,28 @@ outAD.setAttribute('id', 'styleTwo');
 document.body.appendChild(outAD);
 
 ////////////////////////////////////////////////////////////////////////////////
-//
+// # javascript objects and primitives
+// in javascript, object are king. if you understand object, you understand javascript
+// in javascript, almost everything is an object
+// all javascript value, except primitives, are object
+// -> boolean | number | string can be object (if defined with the NEW  keyword)
+// -> dates and maths are always object
+// -> RegExp are always object
+// -> arrays are always object
+// -> functions are always object
+
+// a primitive value is a value that has no (property or method)
+// primitive values are immutable (they are hardcoded) and therefore cannot be changed
+// javascript defines five (5) types of primitive data-types -
+// -> string | number | boolean
+// -> null | undefined
+// NB: (x = 30), any one can change the value of (x), but no one can change the value of (30)
+// NB: primitive examples -
+// -> string -> 'this is a string'
+// -> number -> 11 (any integer or float)
+// -> boolean -> true (true or false)
+// -> null -> null (null is always unll)
+// -> undefined -> undefined (undefined is alwasy undefined)
 
 function ExDA() {
 
